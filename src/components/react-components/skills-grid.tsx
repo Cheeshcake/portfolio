ee85d984794c4aa12065c9806a90cfd4c72f8dd9
Cheeshcake <mohamedazizhadjhassen@gmail.com>
2024-07-12 22:14:04 +0100

    skills section done

---
+++ src/components/react-components/skills-grid.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { skills } from "../../js/data.js";
+
+
+const SkillsGrid = () => {
+  const [invertedIndex, setInvertedIndex] = useState<number>(1);
+
+  const handleImageClick = (index: number) => {
+    setInvertedIndex(index === invertedIndex ? -1 : index);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-5 w-5/6 mx-auto gap-4 md:gap-16 my-14">
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            onClick={() => handleImageClick(index)}
+            className={`p-6 cursor-pointer  h-36  md:h-48 flex flex-col justify-around items-center border-3 border-black rounded-md ${
+              invertedIndex === index
+                ? "bg-black text-white"
+                : "bg-white text-black"
+            }`}
+          >
+            <img
+              src={skill.image}
+              alt={skill.title}
+              className={`${
+                invertedIndex === index ? "invert" : ""
+              }`}
+            />
+            <h2 className="heading-h5-bold">{skill.title}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SkillsGrid;
---
+++ src/js/data.js
@@ -0,0 +1,42 @@
+export const skills = [
+  {
+    image: "/assets/icon-git.png",
+    title: "Git"
+  },
+  {
+    image: "/assets/icon-javascript.png",
+    title: "JavaScript"
+  },
+  {
+    image: "/assets/icon-sass.png",
+    title: "Sass/Scss"
+  },
+  {
+    image: "/assets/icon-nest.png",
+    title: "Nest.Js"
+  },
+  {
+    image: "/assets/icon-storybook.png",
+    title: "Storybook"
+  },
+  {
+    image: "/assets/icon-nest.png",
+    title: "Nest.Js"
+  },
+  {
+    image: "/assets/icon-git.png",
+    title: "Git"
+  },
+  {
+    image: "/assets/icon-storybook.png",
+    title: "Storybook"
+  },
+  {
+    image: "/assets/icon-socket.png",
+    title: "Socket.io"
+  },
+  {
+    image: "/assets/icon-sass.png",
+    title: "Sass/Scss"
+  }
+];
